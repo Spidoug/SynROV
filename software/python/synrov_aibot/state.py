0f450c719b235fdd687f0a2ae98ece7d46ff88c5
Douglas Santana <spidoug@hotmail.com>
2026-08-08 21:08:18 -0300

    Delete software directory

--- software/python/synrov_aibot/state.py
+++
@@ -1,7 +0,0 @@
-"""State dataclasses and pose conversion helpers."""
-from __future__ import annotations
-
-from .helpers import build_armj_command, clamp_member_pose, pose_from_state, ui_pose_to_armj_values
-from .modern_runtime import ModernSynROVState as SynROVState
-
-__all__ = ["SynROVState", "pose_from_state", "ui_pose_to_armj_values", "build_armj_command", "clamp_member_pose"]
